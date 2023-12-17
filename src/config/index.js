d760e7617cac4369c2a8836c51090cff321d5d72
gufernandess <gustavofernandescc@gmail.com>
2023-12-16 22:09:08 -0300

    fix: modificando link de acesso aos dados

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const URL = window.location.hostname.includes('localhost')
 
  ? 'http://localhost:8080'
- : 'https://istudyreact.herokuapp.com';
+ : 'https://istudyreact-daa25f3facad.herokuapp.com';
 
 export default{
     URL,
